873c567f64b4c21271aa7b89e3f9079be75a9b2d
quigg-caroline <quiggc@ucla.edu>
2017-04-16 06:01:32 +0000

    Server timeout, client writing in chunks, server accepting multiple chunks

--- Project1/server.cpp
+++ Project1/server.cpp
@@ -11,13 +11,39 @@
 #include <cstring>
 #include <netdb.h>
 #include <fstream>
+#include <csignal>
 
 #define BACKLOG 10
 using namespace std;
 
+int sockfd;
+int count  = 0;
+struct sockaddr_storage clientAddrs[BACKLOG];
+int client_fds[BACKLOG];
+socklen_t addr_size = sizeof(clientAddrs[count]);
+
+//handle SIGTERM and SIGQUIT
+void sig_handler(int signum)
+{
+  //close open sockets
+  close(sockfd);
+  for(int i = 0 ; i<count ; i++)
+    {
+      close(client_fds[i]);
+    }
+  exit(0);
+}
+
+//read data from a specific client
+
+
 int main(int argc, char** argv)
 {
 
+  //set up handler for SIGQUIT/SIGTERM
+  signal(SIGQUIT, sig_handler);
+  signal(SIGTERM, sig_handler);
+  
   //get addr info
   int status;
   struct addrinfo hints;
@@ -35,7 +61,6 @@ int main(int argc, char** argv)
     }
 
   //create a socket that uses TCP IP
-  int sockfd;
   if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, 0)) < 0 )
     {
       cerr << "ERROR: socket error " << strerror(errno) << endl;
@@ -65,10 +90,6 @@ int main(int argc, char** argv)
     }
 
   //accept new connection(s)
-  int count  = 0;
-  struct sockaddr_storage clientAddrs[BACKLOG];
-  int client_fds[BACKLOG];
-  socklen_t addr_size = sizeof(clientAddrs[count]);
   client_fds[count] = accept(sockfd, (struct sockaddr*) &clientAddrs[count],&addr_size);
   if (client_fds[count] == -1)
     {
@@ -76,6 +97,8 @@ int main(int argc, char** argv)
       exit(1);
     }
   count++;
+
+  //start new thread for client
 
   cout<< "Accepted connection number " << count << endl;
 
@@ -85,24 +108,47 @@ int main(int argc, char** argv)
 
   ofstream file;
   file.open(title);
-  
-  //read from client
-  char buffer[100];
-  memset(&buffer, 0 , sizeof(buffer));
-  read(client_fds[0], buffer, sizeof(char));
 
-  file << buffer;
+  struct timeval timeout;
+  timeout.tv_sec = 10;
+  timeout.tv_usec = 0;
   
-  
-  for (int i =0 ; i< count; i++)
+  //see if there is data from client
+  fd_set set;
+
+  while (1)
     {
-      close(client_fds[i]);
-    }
+      FD_ZERO(&set);
+      FD_SET(client_fds[0], &set);
+  
+      select(client_fds[0] + 1, &set, NULL, NULL, &timeout);
 
-  file.close();
+      if (FD_ISSET(client_fds[0], &set))
+	{
+	  //read from client
+	  char buffer[100];
+	  memset(&buffer, 0 , sizeof(buffer));
+	  
+	  long r  =recv(client_fds[0], buffer, sizeof(buffer),0);
+	  //check if client terminated
+	  if (r == 0)
+	    {
+	      break;
+	    }
+	  file << buffer;
+	}
+
+      else
+	{
+	  file << "ERROR: client aborted";
+	  break;
+	}
+    }
+    close(client_fds[0]);
+    file.close();
   
   //free serv addr
-  close (sockfd);
+  close(sockfd);
   freeaddrinfo(servinfo);
   return 0;
 }
